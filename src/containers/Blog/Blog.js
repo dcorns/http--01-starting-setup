03d252d7f012e58af9b6aee422ed75f13419ceee
Dale Corns <codefellow@gmail.com>
2018-05-28 01:22:18 -0700

    add some post fetching

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,20 +5,33 @@ import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 import axios from 'axios';
-
 class Blog extends Component {
+  state = {
+    posts:[],
+  };
   componentDidMount(){
     axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(res=>console.log(res))
+      .then(res=>{
+        const posts = res.data.splice(0,4);
+        const updatedPosts = posts.map((post) => {
+            return {
+              ...post,
+              author:'Dale'
+            }
+        });
+        this.setState({posts:updatedPosts});
+        console.log(res);
+      })
       .catch(err=>console.log(err));
   }
     render () {
+    const posts = this.state.posts.map(post => {
+      return <Post key={post.id} title={post.title} author={post.author}/>;
+    });
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                  {posts}
                 </section>
                 <section>
                     <FullPost />
